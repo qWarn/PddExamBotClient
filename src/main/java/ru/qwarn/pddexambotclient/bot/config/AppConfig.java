89a46ed9e446a1a67bf107ff3ef202b23e02eed8
qWarn <kirilov-nikitka@list.ru>
2024-02-17 16:44:10 +0300

    Added @EnableAspectJAutoProxy

--- src/main/java/ru/qwarn/pddexambotclient/bot/config/AppConfig.java
+++ src/main/java/ru/qwarn/pddexambotclient/bot/config/AppConfig.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableAspectJAutoProxy
 public class AppConfig {
 
     @Bean
