364648ab53545d066bfc120fd29ac3b698759ac6
qWarn <kirilov-nikitka@list.ru>
2024-02-17 17:20:50 +0300

    Added context loads test

--- src/test/java/ru/qwarn/pddexambotclient/PddExamBotClientApplicationTests.java
+++ src/test/java/ru/qwarn/pddexambotclient/PddExamBotClientApplicationTests.java
@@ -1,13 +1,22 @@
 package ru.qwarn.pddexambotclient;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.qwarn.pddexambotclient.bot.controllers.WebhookController;
+
+import static org.springframework.util.Assert.*;
 
 @SpringBootTest
 class PddExamBotClientApplicationTests {
 
+	@Autowired
+	private WebhookController webhookController;
+
+
 	@Test
 	void contextLoads() {
+		notNull(webhookController, "Context loads failed.");
 	}
 
 }
