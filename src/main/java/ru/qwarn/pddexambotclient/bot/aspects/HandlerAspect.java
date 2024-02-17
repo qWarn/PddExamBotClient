9028c171f518708607a9aea2719d24d498e8a4e1
qWarn <kirilov-nikitka@list.ru>
2024-02-17 16:45:06 +0300

    Added HandlerAspect

---
+++ src/main/java/ru/qwarn/pddexambotclient/bot/aspects/HandlerAspect.java
@@ -0,0 +1,28 @@
+package ru.qwarn.pddexambotclient.bot.aspects;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class HandlerAspect {
+
+
+    @Around(value = "execution(* ru.qwarn.pddexambotclient.bot.handlers.*.*(..))")
+    public Object logHandleMethod(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        log.info("Starting execution of method {} with args {} ", proceedingJoinPoint.getSignature().getName(),
+                proceedingJoinPoint.getArgs());
+
+        Object object = proceedingJoinPoint.proceed();
+        log.info("Ended execution of method {}", proceedingJoinPoint.getSignature().getName());
+        return object;
+    }
+
+
+}
