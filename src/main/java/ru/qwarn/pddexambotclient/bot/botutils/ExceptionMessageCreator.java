1693bd2112492c5a7a0aa2c24b7215c3ed17696f
qWarn <kirilov-nikitka@list.ru>
2024-02-25 21:40:53 +0300

    Updated ExceptionMessageCreator

--- src/main/java/ru/qwarn/pddexambotclient/bot/botutils/ExceptionMessageCreator.java
+++ src/main/java/ru/qwarn/pddexambotclient/bot/botutils/ExceptionMessageCreator.java
@@ -3,44 +3,37 @@ package ru.qwarn.pddexambotclient.bot.botutils;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-
-import java.util.ArrayList;
 import java.util.List;
+
+import static ru.qwarn.pddexambotclient.bot.constants.CallbackConstants.*;
 
 @Component
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExceptionMessageCreator {
     public static SendMessage createExceptionMessage(long chatId, HttpClientErrorException e) {
+        String text = StringUtils.substringBetween(e.getMessage(), "message\":\"", "\",\"path");
 
-        ReplyKeyboard keyboard = null;
-
-        if (e.getStatusCode().equals(HttpStatusCode.valueOf(404))) {
-            keyboard = createBackToTicketsMarkup();
+        if (e.getStatusCode().value() == 404) {
+            return createExceptionMessage(chatId, text, createInlineKeyboard("К билетам", GET_TICKETS));
         }
+        return createExceptionMessage(chatId, text, null);
+    }
 
-        return SendMessage.builder()
-                .chatId(chatId)
-                .text(StringUtils.substringBetween(e.getMessage(), "message\":\"", "\",\"path"))
-                .replyMarkup(keyboard)
+    private static SendMessage createExceptionMessage(long chatId, String text, ReplyKeyboard markup){
+        return SendMessage.builder().chatId(chatId).text(text).replyMarkup(markup).build();
+    }
+
+    private static InlineKeyboardMarkup createInlineKeyboard(String text, String callback){
+        return InlineKeyboardMarkup.builder()
+                .keyboardRow(List.of(InlineKeyboardButton.builder().text(text).callbackData(callback).build()))
                 .build();
     }
 
-    private static InlineKeyboardMarkup createBackToTicketsMarkup() {
-        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
-        InlineKeyboardButton selectedButton = new InlineKeyboardButton();
-        selectedButton.setText("К билетам.");
-        selectedButton.setCallbackData("backToTickets");
-        List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
-        buttons.add(List.of(selectedButton));
-        markup.setKeyboard(buttons);
-        return markup;
-    }
 
 }
