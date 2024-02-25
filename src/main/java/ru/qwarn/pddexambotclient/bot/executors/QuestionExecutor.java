b3514542e38655f97bfa0606e2398351dfdc4ed7
qWarn <kirilov-nikitka@list.ru>
2024-02-25 21:44:26 +0300

    Updated QuestionExecutor

--- src/main/java/ru/qwarn/pddexambotclient/bot/executors/QuestionExecutor.java
+++ src/main/java/ru/qwarn/pddexambotclient/bot/executors/QuestionExecutor.java
@@ -5,21 +5,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.qwarn.pddexambotclient.bot.TelegramBot;
-import ru.qwarn.pddexambotclient.bot.dto.TaskDTO;
 
 import java.util.HashMap;
 
-import static ru.qwarn.pddexambotclient.bot.botutils.RequestConstants.*;
-
+import static ru.qwarn.pddexambotclient.bot.constants.RequestConstants.*;
 @Component
 @RequiredArgsConstructor
 @Setter
@@ -27,41 +24,42 @@ public class QuestionExecutor {
 
     private final ObjectMapper objectMapper;
     private final RestTemplate restTemplate;
+
     private TelegramBot telegramBot;
 
-    public void executeQuestionAnswerAndNextQuestion(long chatId, String answerNumber) throws TelegramApiException, JsonProcessingException {
-        if (answerNumber.matches("^-?\\d+")) {
-                telegramBot.execute(restTemplate.patchForObject(String.format(ANSWER_URI, chatId, answerNumber), null, SendMessage.class));
-                executeQuestion(restTemplate.exchange(
-                        String.format(QUESTION_URI, chatId), HttpMethod.PATCH, null, String.class, new HashMap<>()
-                ));
+    public void executeQuestion(long chatId, String answerNumber, int messageId) throws TelegramApiException, JsonProcessingException {
+        deleteMessage(chatId, messageId);
+        if (answerNumber != null) {
+            restTemplate.patchForObject(String.format(CHECK_ANSWER_URI, chatId, answerNumber), null, SendMessage.class);
+        }
+
+        ResponseEntity<String> questionResponse = restTemplate.exchange(
+                String.format(GET_QUESTION_URI, chatId), HttpMethod.PATCH, null, String.class, new HashMap<>()
+        );
+
+        if (questionResponse.getHeaders().get("MessageType").get(0).equals("sendphoto")) {
+            telegramBot.execute(objectMapper.readValue(questionResponse.getBody(), SendPhoto.class));
         } else {
-            telegramBot.execute(SendMessage.builder().text("Выберите число.").chatId(chatId).build());
+            telegramBot.execute(objectMapper.readValue(questionResponse.getBody(), SendMessage.class));
         }
     }
 
-    private void executeQuestion(ResponseEntity<String> response) throws JsonProcessingException, TelegramApiException {
-        if (response.getHeaders().get("MessageType").get(0).equals("sendphoto")) {
-            telegramBot.execute(objectMapper.readValue(response.getBody(), SendPhoto.class));
-        } else {
-            telegramBot.execute(objectMapper.readValue(response.getBody(), SendMessage.class));
-        }
+    public void executeAnswer(long chatId, int messageId) throws TelegramApiException {
+        deleteMessage(chatId, messageId);
+        telegramBot.execute(
+                restTemplate.getForObject(String.format(GET_CORRECT_ANSWER_URI, chatId), SendMessage.class)
+        );
     }
 
-    public void executeFirstQuestionFromSelected(long chatId) throws TelegramApiException, JsonProcessingException {
-        executeQuestion(restTemplate.exchange(
-                RequestEntity.patch(String.format(QUESTION_URI, chatId)).accept(MediaType.APPLICATION_JSON).body(new TaskDTO("selected", null)),
-                String.class
-        ));
+    public void addQuestionToSelected(long chatId, String questionId) {
+        restTemplate.patchForObject(String.format(ADD_TO_SELECTED_URI, chatId, questionId), null, String.class);
     }
 
-    public void executeFirstQuestionFromTicket(long chatId, int ticketNumber) throws TelegramApiException, JsonProcessingException {
-        executeQuestion(restTemplate.exchange(
-                RequestEntity.patch(String.format(QUESTION_URI, chatId)).accept(MediaType.APPLICATION_JSON).body(new TaskDTO("ticket", ticketNumber)),
-                String.class
-        ));
+    public void removeFromSelectedList(long chatId, String questionId) {
+        restTemplate.patchForObject(String.format(REMOVE_FROM_SELECTED_URI, chatId, questionId), null, String.class);
     }
 
-
-
+    private void deleteMessage(long chatId, int messageId) throws TelegramApiException {
+        telegramBot.execute(DeleteMessage.builder().chatId(chatId).messageId(messageId).build());
+    }
 }
