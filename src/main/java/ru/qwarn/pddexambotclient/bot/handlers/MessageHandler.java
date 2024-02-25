dbf2b297cdc6f8e5b5846e279c5d59d4c867a88e
qWarn <kirilov-nikitka@list.ru>
2024-02-25 21:43:25 +0300

    Updated MessageHandler

--- src/main/java/ru/qwarn/pddexambotclient/bot/handlers/MessageHandler.java
+++ src/main/java/ru/qwarn/pddexambotclient/bot/handlers/MessageHandler.java
@@ -1,32 +1,29 @@
 package ru.qwarn.pddexambotclient.bot.handlers;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import ru.qwarn.pddexambotclient.bot.executors.QuestionExecutor;
 import ru.qwarn.pddexambotclient.bot.executors.TicketExecutor;
 
 @Setter
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Component
 public class MessageHandler implements Handler {
-    private final QuestionExecutor questionExecutor;
+
     private final TicketExecutor ticketExecutor;
 
     @Override
     public void handle(Update update) {
-        String messageFromUser = update.getMessage().getText().toLowerCase();
         long chatId = update.getMessage().getChatId();
+        int messageId = update.getMessage().getMessageId();
+        String message = update.getMessage().getText().toLowerCase();
         try {
-            switch (messageFromUser) {
-                case "/start" -> ticketExecutor.executeStartMessageOrTickets(chatId);
-
-                case "к билетам" -> ticketExecutor.executeTickets(chatId, false);
-
-                default -> questionExecutor.executeQuestionAnswerAndNextQuestion(chatId, messageFromUser);
+            if (message.equals("/start")) {
+                ticketExecutor.executeStartMessageOrTickets(chatId);
+            } else {
+                ticketExecutor.deleteMessage(chatId, messageId);
             }
-
         } catch (Exception e) {
             //ignored
         }
