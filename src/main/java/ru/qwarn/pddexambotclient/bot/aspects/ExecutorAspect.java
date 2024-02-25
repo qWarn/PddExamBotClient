c267e8195a4c7098908aa8e0418fcea5223c73ad
qWarn <kirilov-nikitka@list.ru>
2024-02-25 21:41:04 +0300

    Updated ExecutorAspect

--- src/main/java/ru/qwarn/pddexambotclient/bot/aspects/ExecutorAspect.java
+++ src/main/java/ru/qwarn/pddexambotclient/bot/aspects/ExecutorAspect.java
@@ -1,44 +1,40 @@
 package ru.qwarn.pddexambotclient.bot.aspects;
 
-import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.qwarn.pddexambotclient.bot.TelegramBot;
 import ru.qwarn.pddexambotclient.bot.botutils.ExceptionMessageCreator;
-
-@Lazy
 @Slf4j
 @Aspect
 @Component
-@RequiredArgsConstructor
+@Setter
 public class ExecutorAspect {
 
-    private final TelegramBot telegramBot;
+    private TelegramBot telegramBot;
 
-    @Pointcut("execution(* ru.qwarn.pddexambotclient.bot.executors.*.execute*(..))")
+    @Pointcut("within(ru.qwarn.pddexambotclient.bot.executors.*)")
     public void executorPointcut(){}
 
     @Before("executorPointcut()")
     public void logBefore(JoinPoint joinPoint){
-        log.info("Starting execution of method {} with args {}", joinPoint.getSignature().getName(), joinPoint.getArgs());
+        log.info("Starting execution of method {} with args [{}]", joinPoint.getSignature().getName(), joinPoint.getArgs());
     }
 
-    @AfterThrowing(pointcut = "executorPointcut() && args(long, ..)", throwing = "exception")
+    @AfterThrowing(pointcut = "executorPointcut()", throwing = "exception")
     public void logAndExecuteAfterThrowing(JoinPoint joinPoint, HttpClientErrorException exception) throws TelegramApiException {
         long chatId = (long) joinPoint.getArgs()[0];
-
         log.warn("Caught client error with message: {}", exception.getMessage());
         telegramBot.execute(ExceptionMessageCreator.createExceptionMessage(chatId, exception));
     }
 
     @After("executorPointcut()")
     public void logAfter(JoinPoint joinPoint){
-        log.info("Ended execution of method {} with args {}"
+        log.info("Ended execution of method {} with args [{}]"
                 , joinPoint.getSignature().getName(), joinPoint.getArgs());
     }
 }
