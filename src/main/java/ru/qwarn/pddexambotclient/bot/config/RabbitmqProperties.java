3b63b52ab999e1de44ce4fe59e3573cb755eabb3
qWarn <kirilov-nikitka@list.ru>
2024-02-25 21:37:30 +0300

    Updated RabbitmqProperties

--- src/main/java/ru/qwarn/pddexambotclient/bot/config/RabbitmqProperties.java
+++ src/main/java/ru/qwarn/pddexambotclient/bot/config/RabbitmqProperties.java
@@ -8,7 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "rabbitmq")
+@ConfigurationProperties(prefix = "spring.rabbitmq.notification")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Getter
 @Setter
